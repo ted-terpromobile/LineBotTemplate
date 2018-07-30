05ce616d9fc48ef6b11836b151865bad2199d1ba
Ted <ted@icsgame.com>
2018-07-31 04:59:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -435,17 +435,17 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						template := linebot.NewCarouselTemplate(
 							linebot.NewCarouselColumn(
 								imageURL, "莉亞", displayName + "有什麼事嗎?",
-								linebot.NewPostbackTemplateAction("你是誰?", "自我介紹", "ㄌㄌ你是誰?",),
-								linebot.NewPostbackTemplateAction("測運勢", "測運勢","怎麼測運勢呢?",),
-								linebot.NewPostbackTemplateAction("挑排列組合", "挑排列組合","怎麼挑排列組合呢?",),
-								linebot.NewPostbackTemplateAction("辛苦了，去休息吧。", "exit","辛苦了，去休息吧。",),
+								linebot.NewPostbackTemplateAction("你是誰?", "自我介紹", "ㄌㄌ你是誰?", "ㄌㄌ你是誰?"),
+								linebot.NewPostbackTemplateAction("測運勢", "測運勢","怎麼測運勢呢?","怎麼測運勢呢?"),
+								linebot.NewPostbackTemplateAction("挑排列組合", "挑排列組合","怎麼挑排列組合呢?","怎麼挑排列組合呢?"),
+								linebot.NewPostbackTemplateAction("辛苦了，去休息吧。", "exit","辛苦了，去休息吧。","辛苦了，去休息吧。"),
 							),
 							linebot.NewCarouselColumn(
 								imageURL, "莉亞", "以下是骰子說明喔，因為主要是在玩COC TRPG所以很偏門啦",
-								linebot.NewPostbackTemplateAction("一般擲骰指令", "一般擲骰指令","一般擲骰指令",),
-								linebot.NewPostbackTemplateAction("一般技能指令", "一般技能指令","一般技能指令",),
-								linebot.NewPostbackTemplateAction("SanCheck指令", "SanCheck指令","SanCheck指令",),
-								linebot.NewPostbackTemplateAction("對抗指令", "對抗指令","對抗指令",),
+								linebot.NewPostbackTemplateAction("一般擲骰指令", "一般擲骰指令","一般擲骰指令","一般擲骰指令"),
+								linebot.NewPostbackTemplateAction("一般技能指令", "一般技能指令","一般技能指令","一般技能指令"),
+								linebot.NewPostbackTemplateAction("SanCheck指令", "SanCheck指令","SanCheck指令","SanCheck指令"),
+								linebot.NewPostbackTemplateAction("對抗指令", "對抗指令","對抗指令","對抗指令"),
 							),
 						)
 						if _, err := bot.ReplyMessage(
